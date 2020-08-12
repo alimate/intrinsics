6626cefb70dec30caa2f8aa28bbbbd8db99aa1f3
Ali Dehghani <ali.dehghani.g@gmail.com>
2020-08-13 01:22:19 +0430

    Charming Quark!

---
+++ src/main/java/me/alidg/IntrinsicsBenchmark.java
@@ -0,0 +1,31 @@
+package me.alidg;
+
+import org.openjdk.jmh.Main;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+
+@State(Scope.Benchmark)
+@BenchmarkMode(Mode.Throughput)
+public class IntrinsicsBenchmark {
+
+    @Param("12346545756.54634")
+    double value;
+
+    @Benchmark
+    public double indirect() {
+        return Math.ceil(value);
+    }
+
+    @Benchmark
+    public double direct() {
+        return StrictMath.ceil(value);
+    }
+
+    public static void main(String[] args) throws Exception {
+        Main.main(args);
+    }
+}
